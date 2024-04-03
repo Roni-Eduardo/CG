e94f13687001fb249accc12d2c1212f75c0c36b8
Roni-Eduardo <160971748+Roni-Eduardo@users.noreply.github.com>
2024-04-02 21:18:34 -0300

    aula 27/03/2024

--- Pong/pong.py
+++ Pong/pong.py
@@ -29,8 +29,13 @@ player_1_y = altura // 2 - raquete_altura // 2
 bola_x = largura // 2
 bola_y = altura // 2 - tamanho_bola // 2
 
+#Velocidade da raquete
 raquete_player_1_dy = 5
 raquete_player_dy = 5
+
+#velocidade da bola
+velocidade_bola_x = 2
+velocidade_bola_y = 2
 
 clock = pygame.time.Clock()
 
@@ -42,20 +47,80 @@ while rodando:
 
     screen.fill(PRETO)
 
+    #movendo a bola
+    bola_x += velocidade_bola_x
+    bola_y += velocidade_bola_y
+
+    #Retangulos de colisão
+    bola_rect = pygame.Rect(bola_x, bola_y, tamanho_bola, tamanho_bola)
+    raquete_pc_rect = pygame.Rect(pc_x, pc_y, raquete_largura, raquete_altura)
+    raquete_player_1_rect = pygame.Rect(player_1_x, player_1_y, raquete_largura, raquete_altura)
+
+    #Colisão da bola com as raquetes do pc
+    if bola_rect.colliderect(raquete_pc_rect) or bola_rect.colliderect(raquete_player_1_rect):
+        velocidade_bola_x = -velocidade_bola_x
+
+    #Colisão da bola com as bordas
+    if bola_y <= 0 or bola_y >= altura - tamanho_bola:
+        velocidade_bola_y = -velocidade_bola_y
+
+    #Colisão da bola com as raquetes do player
+    if bola_x < 0:
+        velocidade_bola_x = -velocidade_bola_x
+    if bola_x > largura - tamanho_bola:
+        velocidade_bola_x = -velocidade_bola_x
+
+
+    #Posicionar a bola no inicio do jogo
+        if bola_x <= 0 or bola_x >= largura - tamanho_bola:
+            bola_x = largura // 2 - tamanho_bola // 2
+            bola_y = altura // 2 - tamanho_bola // 2
+            velocidade_bola_x = - velocidade_bola_x
+
+
+    #Movendo a raquete do pc
+    if bola_x < largura // 2:
+        if pc_y < bola_y:
+            pc_y += raquete_player_dy # velocidade da raquete
+        if pc_y > bola_y:
+            pc_y -= raquete_player_dy
+
+
+    #Movendo a raquete do player
+    if bola_x > largura // 2:
+        if player_1_y < bola_y:
+            player_1_y += raquete_player_1_dy # velocidade da raquete
+        if player_1_y > bola_y:
+            player_1_y -= raquete_player_1_dy
+
+    #Evitar o pc sair da area
+    if pc_y < 0:
+        pc_y = 0
+    elif pc_y > altura - raquete_altura:
+        pc_y = altura - raquete_altura
+
+    #Evita o player sair da area
+    if player_1_y < 0:
+        player_1_y = 0
+    elif player_1_y > altura - raquete_altura:
+        player_1_y = altura - raquete_altura
+
+
     pygame.draw.rect(screen, BRANCO, (pc_x, pc_y, raquete_largura, raquete_altura)) #desenha a raquete
     pygame.draw.ellipse(screen, BRANCO, (bola_x, bola_y, tamanho_bola, tamanho_bola)) #desenha a bola
     pygame.draw.rect(screen, BRANCO, (player_1_x, player_1_y, raquete_largura, raquete_altura))
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_UP] and player_1_y > 0:
-        player_1_y -= raquete_player_1_dy
-    if keys[pygame.K_DOWN] and player_1_y < altura - raquete_altura:
-        player_1_y += raquete_player_1_dy
+    #faz a raquete se mover do pc por comandos do teclado
+    #if keys[pygame.K_UP] and player_1_y > 0:
+    #   player_1_y -= raquete_player_1_dy
+    #if keys[pygame.K_DOWN] and player_1_y < altura - raquete_altura:
+    #    player_1_y += raquete_player_1_dy
 
     pygame.display.flip()# atualiza a tela
 
-    clock.tick(120)
+    clock.tick(450)
 
 pygame.quit() # encerra o pygame
 sys.exit() # encerra o sistema
