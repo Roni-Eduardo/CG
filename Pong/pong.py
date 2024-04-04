9ede5505dda2ab602519d7ae7f57ec74e388f65e
Roni-Eduardo <160971748+Roni-Eduardo@users.noreply.github.com>
2024-04-03 21:40:06 -0300

    aula 03/04/2024

--- Pong/pong.py
+++ Pong/pong.py
@@ -37,9 +37,45 @@ raquete_player_dy = 5
 velocidade_bola_x = 2
 velocidade_bola_y = 2
 
+#Score
+score_player_1 = 0
+score_pc = 0
+
+#Configuração da fonte
+font_file = "font/PressStart2P-Regular.ttf"
+font = pygame.font.Font(font_file, 36)
+
 clock = pygame.time.Clock()
 
-rodando = True
+def menu_principal():
+    global rodando
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    rodando = True
+                    return
+                
+        #renderiza o texto do menu
+        screen.fill(PRETO)
+        texto_menu = font.render("Pong", True, BRANCO)
+        text_menu_rect = texto_menu.get_rect(center=(largura // 2, altura // 2))
+        screen.blit(texto_menu, text_menu_rect)
+
+        tempo = pygame.time.get_ticks()
+
+        if tempo % 2000 < 1000:
+            texto_iniciar = font.render("Pressione ESPACO", True, BRANCO)
+            texto_iniciar_rect = texto_iniciar.get_rect(center=(largura // 2, 450))
+            screen.blit(texto_iniciar, texto_iniciar_rect)
+
+        pygame.display.flip()
+
+menu_principal()
+
 while rodando:
     for event in pygame.event.get(): # Loop de eventos
         if event.type == pygame.QUIT:# quando o botão de fechar for pressionado
@@ -72,10 +108,20 @@ while rodando:
 
 
     #Posicionar a bola no inicio do jogo
-        if bola_x <= 0 or bola_x >= largura - tamanho_bola:
+        if bola_x <= 0:
             bola_x = largura // 2 - tamanho_bola // 2
             bola_y = altura // 2 - tamanho_bola // 2
             velocidade_bola_x = - velocidade_bola_x
+            score_player_1 += 1
+            print(f"Score Player_1: {score_player_1}")
+
+    #Posicionar a bola no final do jogo
+        if bola_x >= largura - tamanho_bola:
+            bola_x = largura // 2 - tamanho_bola // 2
+            bola_y = altura // 2 - tamanho_bola // 2
+            velocidade_bola_x = - velocidade_bola_x
+            score_pc += 1
+            print(f"Score Pc: {score_pc}")
 
 
     #Movendo a raquete do pc
@@ -85,13 +131,23 @@ while rodando:
         if pc_y > bola_y:
             pc_y -= raquete_player_dy
 
+    #Mostrando score no jogo
+    fonte_score = pygame.font.Font(font_file, 24)
+    score_texto = fonte_score.render(
+        f"Score Pc: {score_player_1}      Score Player: {score_pc}", True, BRANCO)
+    score_rect = score_texto.get_rect(
+        center=(largura // 2, 30))
+    
+
+    screen.blit(score_texto, score_rect)
+
 
     #Movendo a raquete do player
-    if bola_x > largura // 2:
-        if player_1_y < bola_y:
-            player_1_y += raquete_player_1_dy # velocidade da raquete
-        if player_1_y > bola_y:
-            player_1_y -= raquete_player_1_dy
+    #if bola_x > largura // 2:
+     #   if player_1_y < bola_y:
+      #      player_1_y += raquete_player_1_dy # velocidade da raquete
+       # if player_1_y > bola_y:
+        #    player_1_y -= raquete_player_1_dy
 
     #Evitar o pc sair da area
     if pc_y < 0:
@@ -108,19 +164,20 @@ while rodando:
 
     pygame.draw.rect(screen, BRANCO, (pc_x, pc_y, raquete_largura, raquete_altura)) #desenha a raquete
     pygame.draw.ellipse(screen, BRANCO, (bola_x, bola_y, tamanho_bola, tamanho_bola)) #desenha a bola
-    pygame.draw.rect(screen, BRANCO, (player_1_x, player_1_y, raquete_largura, raquete_altura))
+    pygame.draw.rect(screen, BRANCO, (player_1_x, player_1_y, raquete_largura, raquete_altura))#desenha a raquete
+    pygame.draw.aaline(screen, BRANCO, (largura // 2, 0), (largura // 2, altura))
 
     keys = pygame.key.get_pressed()
 
     #faz a raquete se mover do pc por comandos do teclado
-    #if keys[pygame.K_UP] and player_1_y > 0:
-    #   player_1_y -= raquete_player_1_dy
-    #if keys[pygame.K_DOWN] and player_1_y < altura - raquete_altura:
-    #    player_1_y += raquete_player_1_dy
+    if keys[pygame.K_UP] and player_1_y > 0:
+       player_1_y -= raquete_player_1_dy
+    if keys[pygame.K_DOWN] and player_1_y < altura - raquete_altura:
+        player_1_y += raquete_player_1_dy
 
     pygame.display.flip()# atualiza a tela
 
-    clock.tick(450)
+    clock.tick(200)#fps (velocidade)
 
 pygame.quit() # encerra o pygame
 sys.exit() # encerra o sistema
